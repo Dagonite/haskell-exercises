448e0e8bb764c7f8fa9ef34f8bb8a3dab51e93ee
Dagonite <up821535@myport.ac.uk>
2020-12-16 19:02:13 +0000

    fizzBuzzRange now takes a range argument

--- FizzBuzz/fizzbuzz.hs
+++ FizzBuzz/fizzbuzz.hs
@@ -1,11 +1,14 @@
 -- fizzbuzz.hs
 
 fizzBuzz :: Int -> String
-fizzBuzz n | fizz && buzz = "FizzBuzz"
-           | buzz         = "Buzz"
-           | fizz         = "Fizz"
-           | otherwise    = show n
-           where fizz = mod n 3 == 0
-                 buzz = mod n 5 == 0
+fizzBuzz n
+  | fizz && buzz = "FizzBuzz"
+  | buzz = "Buzz"
+  | fizz = "Fizz"
+  | otherwise = show n
+  where
+    fizz = mod n 3 == 0
+    buzz = mod n 5 == 0
 
-fizzBuzz100 = mapM_ (print . fizzBuzz) [1..100]
+fizzBuzzRange :: Int -> IO ()
+fizzBuzzRange x = mapM_ (print . fizzBuzz) [1 .. x]
